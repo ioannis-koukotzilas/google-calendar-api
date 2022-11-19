e3f6f235417ab39f6db8e8cc18d1bde9e1e41ef6
Ioannis Koukotzilas <ioannis@monoscopic.net>
2022-11-19 16:11:18 +0200

    Modifications

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,11 @@
 /*
-The fetch() API is asynchronous. We need to add the keyword async 
-before the name of the function that uses the fetch API, and add 
-the keyword await before the calls to any asynchronous functions.
-
-TODO: Make the request URL secret.
+Google Calendar API
 */
 
 // Create the top-level function to obtain the JSON using the Fetch API
 async function schedule() {
   // Declare the requestURL variable to store the Google Calendar URL
-  const requestURL = 'https://www.googleapis.com/calendar/v3/calendars/info.kocmoc%40gmail.com/events?key=AIzaSyAIYy9wpMQzj8VAG0BC9hdV538eer5GRjo';
+  const requestURL = 'https://www.googleapis.com/calendar/v3/calendars/kocmoc.collective@gmail.com/events?singleEvents=true&orderBy=startTime&maxResults=2000&key=AIzaSyDyNSG-eHUJ8YrGZRCojAq_AIdUYg4YPWE';
   // Use the URL to initialize a new Request object
   const request = new Request(requestURL);
   // Make the network request using the fetch() function, and this returns a Response object
@@ -30,8 +26,8 @@ function scheduleData(object) {
   const scheduleItems = object.items;
 
   // Create the app title
-  const scheduleTitle = document.createElement('h1');
-  scheduleTitle.textContent = `Google Calendar API`;
+  const scheduleTitle = document.createElement('div');
+  scheduleTitle.textContent = `The list represents confirmed radio shows along with additional metadata such as title, start time / time zone, description, location, etc. API supports single events - recurring events are not valid. Max entries: 2000`;
 
   // Create the list that wraps the schedule items
   const scheduleList = document.createElement('ul');
@@ -48,63 +44,56 @@ function scheduleData(object) {
     const currentDateJSON = currentDate.toJSON();
 
     // Check for upcoming events
-    // if (scheduleItem.end.dateTime > currentDateJSON) {
+    if (scheduleItem.end.dateTime > currentDateJSON) {
+      // Create several HTML elements
+      const scheduleListItem = document.createElement('li');
+      const scheduleListItemTitle = document.createElement('div');
+      const scheduleListItemStartDate = document.createElement('div');
+      // const scheduleListItemEndDate = document.createElement('div');
+      const scheduleListItemDescription = document.createElement('div');
+      const scheduleListItemLocation = document.createElement('div');
 
-    // Create several HTML elements
-    const scheduleListItem = document.createElement('li');
-    const scheduleListItemTitle = document.createElement('div');
-    const scheduleListItemDescription = document.createElement('div');
-    const scheduleListItemLocation = document.createElement('div');
-    const scheduleListItemStartDate = document.createElement('div');
-    const scheduleListItemEndDate = document.createElement('div');
+      // Set attributes
+      scheduleListItemTitle.classList.add('title');
+      scheduleListItemStartDate.classList.add('start-date');
+      // scheduleListItemEndDate.classList.add('end-date');
+      scheduleListItemDescription.classList.add('description');
+      scheduleListItemLocation.classList.add('location');
 
-    // Set attributes
-    scheduleListItemTitle.classList.add('title');
-    scheduleListItemDescription.classList.add('description');
-    scheduleListItemLocation.classList.add('location');
-    scheduleListItemStartDate.classList.add('start-date');
-    scheduleListItemEndDate.classList.add('end-date');
+      // Check if title exists and add content
+      if (scheduleItem.hasOwnProperty('summary')) {
+        scheduleListItemTitle.textContent = `${scheduleItem.summary}`;
+      }
 
-    // Check if title exists and add content
-    if (scheduleItem.hasOwnProperty('summary')) {
-      scheduleListItemTitle.textContent = `Title: ${scheduleItem.summary}`;
-    } else {
-      scheduleListItemTitle.textContent = `Title: No title`;
+      // Add content to start date
+      scheduleListItemStartDate.textContent = `${new Date(scheduleItem.start.dateTime).toString()}`;
+      // Add content to end date
+      // scheduleListItemEndDate.textContent = `End date: ${scheduleItem.end.dateTime}`;
+
+      // Check if description exists and add content
+      if (scheduleItem.hasOwnProperty('description')) {
+        // Strip html style tags from the content
+        const descriptionString = scheduleItem.description;
+        const formattedDescriptionString = descriptionString.replace(/<\/?[^>]+>/gi, '');
+
+        scheduleListItemDescription.textContent = `${formattedDescriptionString}`;
+      }
+
+      // Check if location exists and add content
+      if (scheduleItem.hasOwnProperty('location')) {
+        scheduleListItemLocation.textContent = `${scheduleItem.location}`;
+      }
+
+      // Append list items into the list
+      scheduleList.appendChild(scheduleListItem);
+      // Append elements into each list item
+      scheduleListItem.appendChild(scheduleListItemTitle);
+      scheduleListItem.appendChild(scheduleListItemStartDate);
+      // scheduleListItem.appendChild(scheduleListItemEndDate);
+      scheduleListItem.appendChild(scheduleListItemDescription);
+      scheduleListItem.appendChild(scheduleListItemLocation);
     }
-
-    // Check if description exists and add content
-    if (scheduleItem.hasOwnProperty('description')) {
-      // Strip html style tags from the content
-      const descriptionString = scheduleItem.description;
-      const formattedDescriptionString = descriptionString.replace(/<\/?[^>]+>/gi, '');
-
-      scheduleListItemDescription.textContent = `Description: ${formattedDescriptionString}`;
-    } else {
-      scheduleListItemDescription.textContent = `Description: no description`;
-    }
-
-    // Check if location exists and add content
-    if (scheduleItem.hasOwnProperty('location')) {
-      scheduleListItemLocation.textContent = `Location: ${scheduleItem.location}`;
-    } else {
-      scheduleListItemLocation.textContent = `Location: no location`;
-    }
-
-    // Add content to start date
-    scheduleListItemStartDate.textContent = `Start date: ${scheduleItem.start.dateTime}`;
-    // Add content to end date
-    scheduleListItemEndDate.textContent = `End date: ${scheduleItem.end.dateTime}`;
-
-    // Append list items into the list
-    scheduleList.appendChild(scheduleListItem);
-    // Append elements into each list item
-    scheduleListItem.appendChild(scheduleListItemTitle);
-    scheduleListItem.appendChild(scheduleListItemDescription);
-    scheduleListItem.appendChild(scheduleListItemLocation);
-    scheduleListItem.appendChild(scheduleListItemStartDate);
-    scheduleListItem.appendChild(scheduleListItemEndDate);
   }
-  // }
 }
 
 // Init
